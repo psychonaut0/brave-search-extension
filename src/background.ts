e0f23f48dcf28aa87cf3652933b6f183346e64a1
psychonaut <checco0002@gmail.com>
2024-06-02 10:14:01 +0200

    chore: Removed unused file

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,5 @@
 import browser from "webextension-polyfill";
 
-console.log("Hello from the background!");
-
 browser.runtime.onInstalled.addListener((details) => {
   console.log("Extension installed:", details);
 });
